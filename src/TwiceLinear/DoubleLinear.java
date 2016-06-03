bd563652572bfeb894d94afb6274aec9afe4d42a
alexey novikov <alekseeey@gmail.com>
2016-06-03 10:10:51 +0300

    work on DoubleLinear

--- src/TwiceLinear/DoubleLinear.java
+++ src/TwiceLinear/DoubleLinear.java
@@ -2,6 +2,7 @@ package TwiceLinear;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Iterator;
 
 /**
  * http://www.codewars.com/kata/5672682212c8ecf83e000050
@@ -10,25 +11,49 @@ public class DoubleLinear {
 
     public static int dblLinear(int n) {
         List<Integer> sequence = new ArrayList<Integer>();
-        sequence.add(1);
 
-        int index = 0;
-        while (sequence.size() <= n) {
-            int y = 2 * sequence.get(index) + 1;
-            int z = 3 * sequence.get(index) + 1;
-
-            if (!sequence.contains(y)) {
-                sequence.add(y);
-            }
-
-            if (!sequence.contains(z)) {
-                sequence.add(z);
-            }
-
-            sequence.sort(null);
-
-            index++;
+        int current = 0;
+        for (int i = 1; i <= n / 2; i++) {
+            current = 2 * current + 1;
+            sequence.add(current);
         }
+
+        boolean useZFormula = true;
+
+        int maxNumber = sequence.get(sequence.size() - 1);
+
+        List<Integer> prevSequence = sequence;
+
+        while (prevSequence.size() > 0) {
+            List<Integer> nextSequence = new ArrayList<Integer>();
+
+            Iterator<Integer> iterator = prevSequence.iterator();
+
+            while (iterator.hasNext()) {
+                int calculatedNumber;
+
+                if (useZFormula) {
+                    calculatedNumber = 3 * iterator.next() + 1;
+                } else {
+                    calculatedNumber = 2 * iterator.next() + 1;
+                }
+
+                System.out.println(calculatedNumber);
+
+                if (calculatedNumber < maxNumber) {
+                    nextSequence.add(calculatedNumber);
+//                    sequence.add(calculatedNumber);1!!!!!!!!!!!
+                } else {
+                    useZFormula = false;
+                    maxNumber = calculatedNumber;
+                    break;
+                }
+            }
+
+            prevSequence = nextSequence;
+        }
+
+        sequence.sort(null);
 
         return sequence.get(n);
     }
