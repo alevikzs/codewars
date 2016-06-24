646eb21bf4728dae3afe490008e7cb717c22a374
alexey novikov <alekseeey@gmail.com>
2016-06-24 21:46:40 +0300

    Scramblies done

---
+++ src/Scramblies/Scramblies.java
@@ -0,0 +1,34 @@
+package Scramblies;
+
+/**
+ * https://www.codewars.com/kata/scramblies
+ */
+public class Scramblies {
+
+    public static boolean scramble(String str1, String str2) {
+        boolean result = true;
+
+        for (char char1: str2.toCharArray()) {
+            boolean isCharExist = false;
+
+            for (char char2: str1.toCharArray()) {
+                if (char2 == char1) {
+                    isCharExist = true;
+
+                    str1 = str1.substring(0, str1.indexOf(char2)) + str1.substring(str1.indexOf(char2) + 1);
+
+                    break;
+                }
+            }
+
+            if (!isCharExist) {
+                result = false;
+
+                break;
+            }
+        }
+
+        return result;
+    }
+
+}
