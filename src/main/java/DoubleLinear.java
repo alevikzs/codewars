e12e9615b076f617bfb9c27456b8f6a9c076966d
alevikzs <alekseeey@gmail.com>
2021-01-30 00:33:38 +0200

    added DoubleLinear

---
+++ src/main/java/DoubleLinear.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://www.codewars.com/kata/5672682212c8ecf83e000050
+ */
+class DoubleLinear {
+
+    public static int dblLinear (int n) {
+        final List<Integer> list = new ArrayList<>();
+        list.add(1);
+
+        int index = 0;
+        while (true) {
+            final int y = 2 * list.get(index) + 1;
+
+            if (list.size() > n && list.get(n) < y) {
+                return list.get(n);
+            }
+
+            insert(list, y);
+
+            final int z = 3 * list.get(index) + 1;
+            insert(list, z);
+
+            index++;
+        }
+    }
+
+    private static void insert(final List<Integer> list, final int element) {
+        for (int i = list.size() - 1; i >= 0; i--) {
+            if (list.get(i) < element) {
+                list.add(i + 1, element);
+                break;
+            } else if (list.get(i) == element) {
+                break;
+            }
+        }
+    }
+
+}
