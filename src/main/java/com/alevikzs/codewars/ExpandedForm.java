955e5f5950400aa3d793d9d185d402955ce217b5
Oleksii Novikov <alekseeey@gmail.com>
2021-03-18 02:08:00 +0200

    added ExpandedForm

---
+++ src/main/java/com/alevikzs/codewars/ExpandedForm.java
@@ -0,0 +1,22 @@
+package com.alevikzs.codewars;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * https://www.codewars.com/kata/5842df8ccbd22792a4000245
+ */
+public class ExpandedForm {
+
+    public static String expandedForm(int num) {
+        char[] chars = String.valueOf(num).toCharArray();
+
+        return IntStream.range(0, chars.length)
+                .filter(i -> chars[i] != '0')
+                .mapToObj(i -> String.valueOf(
+                        (int) Math.pow(10, Math.abs(i - (chars.length - 1))) * Character.getNumericValue(chars[i]))
+                )
+                .collect(Collectors.joining(" + "));
+    }
+
+}
---
+++ src/test/java/com/alevikzs/codewars/ExpandedFormTest.java
@@ -0,0 +1,16 @@
+package com.alevikzs.codewars;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ExpandedFormTest {
+
+    @Test
+    public void testSomething() {
+//        assertEquals("10 + 2", ExpandedForm.expandedForm(12));
+//        assertEquals("40 + 2", ExpandedForm.expandedForm(42));
+        assertEquals("70000 + 300 + 4", ExpandedForm.expandedForm(70304));
+    }
+
+}
