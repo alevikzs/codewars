76dda0c2438a3e218c750183cbfb2eac57049e99
Oleksii Novikov <alekseeey@gmail.com>
2023-01-05 23:55:44 +0200

    refactoring tests

--- src/test/java/com/alevikzs/codewars/PangramTest.java
+++ src/test/java/com/alevikzs/codewars/PangramTest.java
@@ -2,7 +2,8 @@ package com.alevikzs.codewars;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PangramTest {
 
@@ -10,14 +11,14 @@ public class PangramTest {
     public void test1() {
         String pangram1 = "The quick brown fox jumps over the lazy dog.";
         PangramChecker pc = new PangramChecker();
-        assertEquals(true, pc.check(pangram1));
+        assertTrue(pc.check(pangram1));
     }
 
     @Test
     public void test2() {
         String pangram2 = "You shall not pass!";
         PangramChecker pc = new PangramChecker();
-        assertEquals(false, pc.check(pangram2));
+        assertFalse(pc.check(pangram2));
     }
 
 }
